cc9a09e4a0de8df494c984b06c3a905db1c1009f
Don.CorliJoni <55767309+Don-CorliJoni@users.noreply.github.com>
2023-07-03 15:09:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from logging_helper import get_logger, close_logging
 from Configuration import get_configuration
 from Automation import Automation
 
-
 logger = get_logger(__name__)
 
 
@@ -16,19 +15,21 @@ def config_generator():
     """
     cwd = os.getcwd()
     for filename in filter(lambda file: file.endswith('.json'), os.listdir(cwd)):
+        # Yield each filename and its corresponding loaded configuration
         yield filename, get_configuration(os.path.join(cwd, filename))
-
 
 
 def main():
     for filename, configuration in config_generator():
-
+        # Check if the configuration has the attribute "use_config" and its value is True
         if hasattr(configuration, "use_config") and getattr(configuration, "use_config") is True:
+            # Create an instance of the Automation class with the current configuration
             automation = Automation(configuration, filename)
             try:
                 automation.open_hyperlink()
                 automation.run()
             except Exception as e:
+                # Log the error including stack trace
                 logger.critical(f'Automation failed!\n{automation.get_process_information()}', exc_info=True)
                 automation.close()
                 raise e
