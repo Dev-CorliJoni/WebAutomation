b21a66b2c641696193dce41d86df48f4105ec3a6
Don.CorliJoni <55767309+Don-CorliJoni@users.noreply.github.com>
2023-07-04 13:49:33 +0200

    Update BaseAutomationStep.py

--- Automation/AutomationSteps/BaseAutomationStep.py
+++ Automation/AutomationSteps/BaseAutomationStep.py
@@ -1,9 +1,16 @@
 import os
 
-
 class BaseAutomationStep:
+    """
+    Base class for automation steps.
+    """
 
     @staticmethod
     def validate_filepath(path):
+        """
+        Validates if the given path is a valid file.
+
+        :param path: The path to validate.
+        """
         if not os.path.isfile(path):
             raise Exception(f"Path is invalid '{path}'!")
