f60189cd0ae6916fa104bdf6a3f4020c27331032
Don.CorliJoni <55767309+Don-CorliJoni@users.noreply.github.com>
2023-07-04 13:25:49 +0200

    Update checker.py

--- Configuration/checker.py
+++ Configuration/checker.py
@@ -1,5 +1,25 @@
+import json
+import jsonschema
+
+
 class ConfigurationChecker:
 
-    def __init__(self):
-        pass
+    def __init__(self, schema_path):
+        """    
+        :param schema_path: The path to the JSON schema file.
+        """
+        self.schema = json.load(schema_file)
 
+    def validate(self, json_data):
+        """
+        Validates the given JSON data against a JSON schema.
+    
+        :param json_data: The JSON data to validate.
+        :return: True if the JSON data is valid according to the schema, False otherwise.
+        """
+        try:
+            jsonschema.validate(json_data, self.schema)
+            return True
+        except Exception as e:
+            print(f"JSON validation error: {str(e)}")
+            return False
