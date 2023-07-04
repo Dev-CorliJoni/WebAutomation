f00bc5443f866ecfb90b038a66d85c96967fd2ee
Don.CorliJoni <55767309+Don-CorliJoni@users.noreply.github.com>
2023-07-04 13:32:18 +0200

    Update helper.py

--- Automation/helper.py
+++ Automation/helper.py
@@ -1,2 +1,9 @@
 def _has_attributes(obj, *attributes):
+    """
+    Checks if an object has all the specified attributes.
+
+    :param obj: The object to check for attributes.
+    :param attributes: Variable number of attributes to check.
+    :return: True if the object has all the specified attributes, False otherwise.
+    """
     return all([hasattr(obj, attribute) for attribute in attributes])
