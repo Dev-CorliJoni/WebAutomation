e5f8f64d7d5be2776030e76e6f65044892c206cb
Don.CorliJoni <55767309+Don-CorliJoni@users.noreply.github.com>
2023-07-04 13:30:59 +0200

    Update configuration_loader.py

--- Configuration/configuration_loader.py
+++ Configuration/configuration_loader.py
@@ -4,6 +4,12 @@ from Configuration import Configuration, ConfigurationChecker
 
 
 def get_configuration(path):
+    """
+    Loads a configuration from a JSON file.
+
+    :param path: Path to the JSON file.
+    :return: Configuration object representing the loaded configuration.
+    """
     abs_path = os.path.abspath(path)
 
     with open(abs_path, 'r') as f:
