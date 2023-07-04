40640fbbd2da538f9691ba10fffde3c458098d7d
Don.CorliJoni <55767309+Don-CorliJoni@users.noreply.github.com>
2023-07-04 13:48:41 +0200

    Update change_configuration_automation_step.py

--- Automation/AutomationSteps/change_configuration_automation_step.py
+++ Automation/AutomationSteps/change_configuration_automation_step.py
@@ -1,15 +1,30 @@
 from Automation.AutomationSteps.BaseAutomationStep import BaseAutomationStep
 from Configuration import get_configuration
-from logging_helper import  get_logger
+from logging_helper import get_logger
 
 logger = get_logger(__name__)
 
 class ChangeConfigurationAutomationStep(BaseAutomationStep):
+    """
+    Class representing an automation step to change the configuration.
+    """
+
     def __init__(self, automation_step_data):
+        """
+        Initialize the ChangeConfigurationAutomationStep object.
+
+        :param automation_step_data: The data for the automation step.
+        """
         self.configuration = automation_step_data.change_configuration
         self.validate_filepath(self.configuration)
 
     def __call__(self, *args, **kwargs):
+        """
+        Execute the change configuration step.
+
+        :param args: Additional arguments.
+        :param kwargs: Additional keyword arguments.
+        """
         automation, session = args
         configuration = get_configuration(self.configuration)
 
