13be5a1db14b44ae48407c51a089b247dd5ef734
Don.CorliJoni <haueis.jonathan@gmail.com>
2023-07-09 00:23:55 +0200

    Accepting no or --configuration attribute

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import os
+import argparse
 
 from Configuration import get_configuration
 from Automation import Automation
 from logging_helper import get_logger, close_logging
 
 logger = get_logger(__name__)
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Evaluate arguments provided to the script.')
+    parser.add_argument('-c', '--configuration',type=str, help='Path to json configuration')
+
+    args = parser.parse_args()
+    return args.configuration
 
 
 def config_generator():
@@ -19,8 +28,8 @@ def config_generator():
         yield filename, get_configuration(os.path.join(cwd, filename))
 
 
-def main():
-    for filename, configuration in config_generator():
+def main(configurations):
+    for filename, configuration in configurations:
         # Check if the configuration has the attribute "use_config" and its value is True
         if hasattr(configuration, "use_config") and getattr(configuration, "use_config") is True:
             # Create an instance of the Automation class with the current configuration
@@ -38,4 +47,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    path = get_arguments()
+
+    if path is None:
+        main(config_generator())
+    else:
+        main([(path, get_configuration(path))])
