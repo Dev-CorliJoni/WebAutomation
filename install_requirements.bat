4f97571210126105086d12ac144a0d6688e40588
Don.CorliJoni <haueis.jonathan@gmail.com>
2023-07-09 16:38:02 +0200

    Improved start and install_requirements batch

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,2 +1,22 @@
-pip install -r requirements.txt
+@ECHO OFF
+
+:venv_configured_question
+cls
+set /p "venv_configured=Do you have configured a venv in this repo(y/n): "
+
+IF NOT "%venv_configured%"=="y" IF NOT "%venv_configured%"=="n" GOTO venv_configured_question
+
+IF "%venv_configured%"=="y" SET python = .\venv\Scripts\python.exe
+IF "%venv_configured%"=="n" SET python = python
+
+cls
+%python % -m pip install -r requirements.txt
+
+IF "%ERRORLEVEL%"=="0" GOTO END
+
+set /p "python_new_quoted=Enter the path to your python exe: "
+
+%python_new_quoted% -m pip install -r requirements.txt
+
+:END
 PAUSE
--- start_automation.bat
+++ start_automation.bat
@@ -1,8 +1,33 @@
 @ECHO OFF
 
-SET python = .\venv\Scripts\python.exe
+:venv_configured_question
+cls
+set /p "venv_configured=Do you have configured a venv in this repo(y/n): "
+
+IF NOT "%venv_configured%"=="y" IF NOT "%venv_configured%"=="n" GOTO venv_configured_question
+
+IF "%venv_configured%"=="y" SET python = .\venv\Scripts\python.exe
+IF "%venv_configured%"=="n" SET python = python
+
+cls
 SET script = .\main.py
 
-%python % %script %
+set /p "configuration_path=Enter the configuration file you want to start: "
+GOTO execute_python
 
+:ask_for_python_path
+
+set /p "python_new_quoted=Enter the path to your python exe: "
+IF %python_new_quoted:~0,1%%python_new_quoted:~-1% == "" SET python_new=%python_new_quoted:~1,-1%
+IF NOT %python_new_quoted:~0,1%%python_new_quoted:~-1% == "" SET python_new=%python_new_quoted%
+
+%python_new % %script % --configuration %configuration_path%
+GOTO END
+
+:execute_python
+%python % %script % --configuration %configuration_path%
+
+IF NOT "%ERRORLEVEL%"=="0" GOTO ask_for_python_path
+
+:END
 PAUSE
